a4721289920210339aea66e67965983ae2b3da8e
Sarit Dutta <saritd107@gmail.com>
2020-06-09 07:55:38 -0400

    Changes to use Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherSearch.addEventListener('submit', (event) => {
     m1.textContent = "Loading data."
     m2.textContent = ""
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             m1.textContent = data.error
